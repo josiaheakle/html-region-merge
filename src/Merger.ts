3eeead7458c11624806e55ce21c0ed3102da46dc
henryotoole (dev server) <josiaheakle@gmail.com>
2022-07-12 20:52:05 +0000

    updating to npm

--- src/Merger.ts
+++ src/Merger.ts
@@ -1,113 +1,113 @@
-import * as fs from "fs";
+import * as fs from "fs"
 
 class Merger {
-  private config: {
-    templateDir: string;
-    entry: string;
-    build: string;
-    runScript: string;
-  };
+	private config: {
+		templateDir: string
+		entry: string
+		build: string
+		runScript: string
+	}
 
-  constructor() {
-    this.config = getConfig();
-  }
+	constructor() {
+		this.config = getConfig()
+	}
 
-  mergeDocument(path?: string) {
-    console.log(`Merging documents!`);
-    const mainHTML = fs.readFileSync(path ?? this.config.entry, { encoding: "utf8" });
-    const htmlContent = mainHTML.split("\n");
+	mergeDocument(path?: string) {
+		console.log(`Merging documents!`)
+		const mainHTML = fs.readFileSync(path ?? this.config.entry, { encoding: "utf8" })
+		const htmlContent = mainHTML.split("\n")
 
-    const regionRegEx = /<region.*\/>/g;
-    const nameRegEx = /name=("|').*("|')/g;
+		const regionRegEx = /<region.*\/>/g
+		const nameRegEx = /name=("|').*("|')/g
 
-    let newContent: Array<string> = [];
+		let newContent: Array<string> = []
 
-    // Check each line of the html document for <region />
-    // if no region on that line, add that line to the new html document.
-    // If there is a region on that line, check the template directory for specified region.
-    let noRegions = true;
-    for (const line of htmlContent) {
-      if (line.match(regionRegEx)) {
-        noRegions = false;
-        for (const region of line.matchAll(regionRegEx)) {
-          const nameIndex = region.toString().indexOf("name");
-          let dirIndex = region.toString().indexOf("dir");
+		// Check each line of the html document for <region />
+		// if no region on that line, add that line to the new html document.
+		// If there is a region on that line, check the template directory for specified region.
+		let noRegions = true
+		for (const line of htmlContent) {
+			if (line.match(regionRegEx)) {
+				noRegions = false
+				for (const region of line.matchAll(regionRegEx)) {
+					const nameIndex = region.toString().indexOf("name")
+					let dirIndex = region.toString().indexOf("dir")
 
-          let nameStr;
-          let dirStr;
-          if (dirIndex === -1) {
-            nameStr = region.toString().match(nameRegEx).toString();
-          } else {
-            nameStr = region.toString().substring(nameIndex, dirIndex);
-            dirStr = region.toString().substring(dirIndex);
-          }
+					let nameStr
+					let dirStr
+					if (dirIndex === -1) {
+						nameStr = region.toString().match(nameRegEx).toString()
+					} else {
+						nameStr = region.toString().substring(nameIndex, dirIndex)
+						dirStr = region.toString().substring(dirIndex)
+					}
 
-          const name = nameStr
-            .replace(/ /g, "")
-            .replace(/'/g, "")
-            .replace(/name=/g, "")
-            .replace(/"/g, "");
+					const name = nameStr
+						.replace(/ /g, "")
+						.replace(/'/g, "")
+						.replace(/name=/g, "")
+						.replace(/"/g, "")
 
-          const dir = dirStr
-            ? dirStr
-                .replace(/ /g, "")
-                .replace(/\/>/g, "")
-                .replace(/'/g, "")
-                .replace(/dir=/g, "")
-                .replace(/"/g, "")
-            : "";
+					const dir = dirStr
+						? dirStr
+								.replace(/ /g, "")
+								.replace(/\/>/g, "")
+								.replace(/'/g, "")
+								.replace(/dir=/g, "")
+								.replace(/"/g, "")
+						: ""
 
-          console.log(`Adding ${dir}/${name}.region.html`);
+					console.log(`Adding ${dir}/${name}.region.html`)
 
-          const htmlContent = this.getHTMLTemplate(name, dir === "" ? undefined : dir);
-          newContent.push(htmlContent);
-        }
-      } else {
-        newContent.push(line);
-      }
-    }
+					const htmlContent = this.getHTMLTemplate(name, dir === "" ? undefined : dir)
+					newContent.push(htmlContent)
+				}
+			} else {
+				newContent.push(line)
+			}
+		}
 
-    fs.writeFileSync(this.config.build, newContent.join("\n"), {
-      encoding: "utf8",
-      flag: "w",
-    });
+		fs.writeFileSync(this.config.build, newContent.join("\n"), {
+			encoding: "utf8",
+			flag: "w",
+		})
 
-    if (noRegions === false) this.mergeDocument(this.config.build);
-  }
+		if (noRegions === false) this.mergeDocument(this.config.build)
+	}
 
-  getHTMLTemplate(name: string, dir?: string) {
-    let filePath = dir
-      ? `${this.config.templateDir}/${dir}/${name}.region.html`
-      : `${this.config.templateDir}/${name}.region.html`;
-    try {
-      return fs.readFileSync(filePath, {
-        encoding: "utf8",
-      });
-    } catch {
-      console.log(`No file found while searching for region document ${filePath}`);
-    }
-  }
+	getHTMLTemplate(name: string, dir?: string) {
+		let filePath = dir
+			? `${this.config.templateDir}/${dir}/${name}.region.html`
+			: `${this.config.templateDir}/${name}.region.html`
+		try {
+			return fs.readFileSync(filePath, {
+				encoding: "utf8",
+			})
+		} catch {
+			console.log(`No file found while searching for region document ${filePath}`)
+		}
+	}
 }
 
 export const getConfig = () => {
-  const rootDir = process.env.INIT_CWD;
-  let file;
-  try {
-    file = fs.readFileSync(`${rootDir}/rfm-config.json`, {
-      encoding: "utf8",
-    });
-  } catch {
-    console.log(
-      `Missing configuration file. Must be named "rfm-config.json" and must exist in the project's root directory.`
-    );
-  }
-  let config;
-  try {
-    config = JSON.parse(file);
-  } catch {
-    console.log(`Configuration file in invalid format. "rfm-config.json" must be valid JSON.`);
-  }
-  return config;
-};
+	const rootDir = process.env.INIT_CWD
+	let file
+	try {
+		file = fs.readFileSync(`${rootDir}/rfm-config.json`, {
+			encoding: "utf8",
+		})
+	} catch {
+		console.log(
+			`Missing configuration file. Must be named "rfm-config.json" and must exist in the project's root directory.`
+		)
+	}
+	let config
+	try {
+		config = JSON.parse(file)
+	} catch {
+		console.log(`Configuration file in invalid format. "rfm-config.json" must be valid JSON.`)
+	}
+	return config
+}
 
-export default Merger;
+export default Merger
