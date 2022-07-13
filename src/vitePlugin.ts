7402de399c69751fc8984238ca740260a7a29e3d
henryotoole (dev server) <jreed73@vols.utk.edu>
2022-07-13 15:07:31 +0000

    Change to only run script if HTML document changed.

--- src/vitePlugin.ts
+++ src/vitePlugin.ts
@@ -4,14 +4,17 @@ import { exec } from "child_process";
 import { getConfig } from "./Merger";
 
 export default function hmrPlugin() {
-  return {
-    name: `HMR HTML Merger`,
-    handleHotUpdate(opts: any): [] {
-      const config = getConfig();
-      const rootDir = process.env.INIT_CWD;
-      if (opts.file === path.join(rootDir, config.build)) return;
-      exec(config.runScript);
-      return [];
-    },
-  };
+	return {
+		name: `HMR HTML Merger`,
+		handleHotUpdate(opts: any): [] {
+			const config = getConfig();
+			const rootDir = process.env.INIT_CWD;
+			const fileExt = opts.file.split(".").pop();
+			if (fileExt.toLowerCase() === "html") {
+				if (opts.file === path.join(rootDir, config.build)) return;
+				exec(config.runScript);
+			}
+			return [];
+		},
+	};
 }
