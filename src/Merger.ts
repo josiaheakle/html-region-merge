efd681daec16da7e80c49bf89082385bdd092690
henryotoole (dev server) <josiaheakle@gmail.com>
2022-07-13 14:34:30 +0000

    Updated to only pass through once.

--- src/Merger.ts
+++ src/Merger.ts
@@ -1,113 +1,125 @@
-import * as fs from "fs";
+import * as fs from "fs"
+
+const regionRegEx = /<region.*\/>/g
+const nameRegEx = /name=("|').*("|')/g
 
 class Merger {
-  private config: {
-    templateDir: string;
-    entry: string;
-    build: string;
-    runScript: string;
-  };
+	private config: {
+		templateDir: string
+		entry: string
+		build: string
+		runScript: string
+	}
 
-  constructor() {
-    this.config = getConfig();
-  }
+	constructor() {
+		this.config = getConfig()
+	}
 
-  mergeDocument(path?: string) {
-    console.log(`Merging documents!`);
-    const mainHTML = fs.readFileSync(path ?? this.config.entry, { encoding: "utf8" });
-    const htmlContent = mainHTML.split("\n");
+	/**
+	 * Loops through entry file, checking for any <region /> elements.
+	 * Gets specified HTML document, checking internally for any region elements before merging to the main HTML document.
+	 * ---
+	 * @param path Document to merge. Will check this.config.entry if nothing is passed. Used to pass back through the build file (this functionality should be removed).
+	 */
+	mergeDocument(path?: string) {
+		const mainHTML = fs.readFileSync(path ?? this.config.entry, { encoding: "utf8" })
 
-    const regionRegEx = /<region.*\/>/g;
-    const nameRegEx = /name=("|').*("|')/g;
+		let newContent: string = this.replaceRegions(mainHTML)
 
-    let newContent: Array<string> = [];
+		fs.writeFileSync(this.config.build, newContent, {
+			encoding: "utf8",
+			flag: "w",
+		})
+	}
 
-    // Check each line of the html document for <region />
-    // if no region on that line, add that line to the new html document.
-    // If there is a region on that line, check the template directory for specified region.
-    let noRegions = true;
-    for (const line of htmlContent) {
-      if (line.match(regionRegEx)) {
-        noRegions = false;
-        for (const region of line.matchAll(regionRegEx)) {
-          const nameIndex = region.toString().indexOf("name");
-          let dirIndex = region.toString().indexOf("dir");
+	/**
+	 * Loops through each line of an HTML document, recursively replacing each iteration of a region with the html it references.
+	 * ---
+	 * @returns Merged HTML string
+	 */
+	replaceRegions(html: string): string {
+		const htmlContent = html.split("\n")
+		const newContent: Array<string> = []
+		// Check each line of the html document for <region />
+		// if no region on that line, add that line to the new html document.
+		// If there is a region on that line, check the template directory for specified region.
+		for (const line of htmlContent) {
+			if (line.match(regionRegEx)) {
+				// noRegions = false;
+				for (const region of line.matchAll(regionRegEx)) {
+					const nameIndex = region.toString().indexOf("name")
+					let dirIndex = region.toString().indexOf("dir")
 
-          let nameStr;
-          let dirStr;
-          if (dirIndex === -1) {
-            nameStr = region.toString().match(nameRegEx).toString();
-          } else {
-            nameStr = region.toString().substring(nameIndex, dirIndex);
-            dirStr = region.toString().substring(dirIndex);
-          }
+					let nameStr
+					let dirStr
+					if (dirIndex === -1) {
+						nameStr = region.toString().match(nameRegEx).toString()
+					} else {
+						nameStr = region.toString().substring(nameIndex, dirIndex)
+						dirStr = region.toString().substring(dirIndex)
+					}
 
-          const name = nameStr
-            .replace(/ /g, "")
-            .replace(/'/g, "")
-            .replace(/name=/g, "")
-            .replace(/"/g, "");
+					const name = nameStr
+						.replace(/ /g, "")
+						.replace(/'/g, "")
+						.replace(/name=/g, "")
+						.replace(/"/g, "")
 
-          const dir = dirStr
-            ? dirStr
-                .replace(/ /g, "")
-                .replace(/\/>/g, "")
-                .replace(/'/g, "")
-                .replace(/dir=/g, "")
-                .replace(/"/g, "")
-            : "";
+					const dir = dirStr
+						? dirStr
+								.replace(/ /g, "")
+								.replace(/\/>/g, "")
+								.replace(/'/g, "")
+								.replace(/dir=/g, "")
+								.replace(/"/g, "")
+						: ""
 
-          console.log(`Adding ${dir}/${name}.region.html`);
+					// Got content of HTML document being referenced.
+					// Now to check this content before adding it to the new content.
+					const replaceContent = this.getHTMLTemplate(name, dir === "" ? undefined : dir)
+					const regionContent = this.replaceRegions(replaceContent)
+					newContent.push(regionContent)
+				}
+			} else {
+				newContent.push(line)
+			}
+		}
+		return newContent.join("\n")
+	}
 
-          const htmlContent = this.getHTMLTemplate(name, dir === "" ? undefined : dir);
-          newContent.push(htmlContent);
-        }
-      } else {
-        newContent.push(line);
-      }
-    }
-
-    fs.writeFileSync(this.config.build, newContent.join("\n"), {
-      encoding: "utf8",
-      flag: "w",
-    });
-
-    if (noRegions === false) this.mergeDocument(this.config.build);
-  }
-
-  getHTMLTemplate(name: string, dir?: string) {
-    let filePath = dir
-      ? `${this.config.templateDir}/${dir}/${name}.region.html`
-      : `${this.config.templateDir}/${name}.region.html`;
-    try {
-      return fs.readFileSync(filePath, {
-        encoding: "utf8",
-      });
-    } catch {
-      console.log(`No file found while searching for region document ${filePath}`);
-    }
-  }
+	getHTMLTemplate(name: string, dir?: string) {
+		let filePath = dir
+			? `${this.config.templateDir}/${dir}/${name}.region.html`
+			: `${this.config.templateDir}/${name}.region.html`
+		try {
+			return fs.readFileSync(filePath, {
+				encoding: "utf8",
+			})
+		} catch {
+			console.log(`No file found while searching for region document ${filePath}`)
+		}
+	}
 }
 
 export const getConfig = () => {
-  const rootDir = process.env.INIT_CWD;
-  let file;
-  try {
-    file = fs.readFileSync(`${rootDir}/rfm-config.json`, {
-      encoding: "utf8",
-    });
-  } catch {
-    console.log(
-      `Missing configuration file. Must be named "rfm-config.json" and must exist in the project's root directory.`
-    );
-  }
-  let config;
-  try {
-    config = JSON.parse(file);
-  } catch {
-    console.log(`Configuration file in invalid format. "rfm-config.json" must be valid JSON.`);
-  }
-  return config;
-};
+	const rootDir = process.env.INIT_CWD
+	let file
+	try {
+		file = fs.readFileSync(`${rootDir}/rfm-config.json`, {
+			encoding: "utf8",
+		})
+	} catch {
+		console.log(
+			`Missing configuration file. Must be named "rfm-config.json" and must exist in the project's root directory.`
+		)
+	}
+	let config
+	try {
+		config = JSON.parse(file)
+	} catch {
+		console.log(`Configuration file in invalid format. "rfm-config.json" must be valid JSON.`)
+	}
+	return config
+}
 
-export default Merger;
+export default Merger
